7d23a18e5ab5f96cb2e6ff0f3d40f9a35f4f4ee9
Adam Neumann <noizwaves@users.noreply.github.com>
2018-06-18 14:08:36 -0400

    Adding service security

---
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
@@ -0,0 +1,36 @@
+package io.pivotal.pal.tracker.oauthserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class SecurityConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final String secret;
+
+    public SecurityConfig(@Value("${oauth.sso.secret:supersecret}") String secret) {
+        this.secret = secret;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("tracker-client")
+                .secret(this.secret)
+                .authorizedGrantTypes("client_credentials")
+                .scopes("openid");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+}
